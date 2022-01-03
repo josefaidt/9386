2c121461eb1bd1a59faf54425c8280622ff6ad8f
josefaidt <josef.aidt@gmail.com>
2022-01-03 14:02:22 -0600

    init layer and function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "93869f04a51a": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "9386layera1fe368f": {
+            "Arn": "string"
+        }
+    }
+}
